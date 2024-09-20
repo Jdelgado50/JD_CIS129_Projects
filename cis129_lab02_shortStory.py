d8c241150bb6078489333a7b370c09c62314bbf0
Jdelgado50 <jdelgado50@mail.pima.edu>
2024-09-19 20:49:57 -0700

    Erased hello world and added short story on PRINTS

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Student McLearner
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+
+print("There was a college student that wanted to study programming")
+print("He came to class in time, read the book and did all of the assignments in time")
+print("He then aced all of his classes")
+print("Graduated and Became a fluent programmer"
+print("The End")
