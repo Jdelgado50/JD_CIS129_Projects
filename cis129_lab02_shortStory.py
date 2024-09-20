dc586c8382b01d16dc293b2d0c2b8b0eabe77bbd
Jdelgado50 <jdelgado50@mail.pima.edu>
2024-09-19 20:53:04 -0700

    Erased hello world and added short story on PRINTS

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -5,5 +5,5 @@
 print("There was a college student that wanted to study programming")
 print("He came to class in time, read the book and did all of the assignments in time")
 print("He then aced all of his classes")
-print("Graduated and Became a fluent programmer"
+print("Graduated and Became a fluent programmer")
 print("The End")
